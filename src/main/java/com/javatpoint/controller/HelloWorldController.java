0c6a83174b1064d7a0b2b026a99cd83cb5cb6d95
anubhavPOC <111060282+anubhavPOC@users.noreply.github.com>
2023-06-29 14:44:21 +0530

    Update HelloWorldController.java

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.javatpoint.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello Anubhav";
-}
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String getMessage()
+    {
+        return "Hello World!";
+    }
 }
